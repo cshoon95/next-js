960587fdb891fe10da370bb3e858ab39b3202b38
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-06-27 20:37:03 +0900

    [헤더] navBar modules css 추가

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css"; 
 
 export default function NavBar() {
     const router = useRouter();
@@ -7,8 +8,8 @@ export default function NavBar() {
     return (
         // Link를 사용하지 않고 anchor태그를 사용해서 한다면 페이지를 전체 리로드한다.
         <nav>
-            <Link href="/"><a style={{color: router.pathname === "/" ? "red" : "blue"}}>Home</a></Link>
-            <Link href="/about"><a style={{color: router.pathname === "/" ? "red" : "blue"}}>About</a></Link>
+            <Link href="/"><a className={`${styles.link} ${router.pathname === '/' ? styles.active : ""}`}>Home</a></Link>
+            <Link href="/about"><a className={`${styles.link} ${router.pathname === '/about' ? styles.active : ""}`}>About</a></Link>
         </nav>
     )
 }
