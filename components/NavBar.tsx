6055f56211e2fb35fb86035d5aea80474d73a669
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-06-28 21:39:05 +0900

    custom app 생성으로 인한 리팩토링

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,17 +9,6 @@ export default function NavBar() {
         <nav>
             <Link href="/"><a className={router.pathname === "/" ? "active" : ""}>Home</a></Link>
             <Link href="/about"><a className={router.pathname === "/about" ? "active" : ""}>About</a></Link>
-            <style jsx>{`
-                nav {
-                    background-color: tomato;
-                }
-                a {
-                    text-decoration: none;
-                }
-                .active {
-                    color: yellow;
-                }`
-            }</style>
         </nav>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import NavBar from "../components/NavBar";
 export default function Home() {
     return (
         <div>
-            <NavBar/>
             <h1>hello</h1>
         </div>
     );
