409ab45e915ecd2e7c0831a0f9f607fe1d6a0bc1
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-06-30 21:43:49 +0900

    [홈] 영화 포스터 스타일 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,15 +39,39 @@ export default function Home() {
 
     return (
         <>
-        <Seo title="Home"/>
-        {movies.length === 0 && <h4>Loading...</h4>}
-        {movies?.map((movie: IMovieProps) => {
-            return (
-                <div key={movie.id}>
-                    <h4>{movie.original_title}</h4>
-                </div>
-            )
-        })}
+        <div className="container">
+            <Seo title="Home"/>
+            {movies.length === 0 && <h4>Loading...</h4>}
+            {movies?.map((movie: IMovieProps) => {
+                return (
+                    <div key={movie.id}>
+                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                        <h4>{movie.original_title}</h4>
+                    </div>
+                )
+            })}
+        </div>
+        <style jsx>{`
+            .container {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                padding: 20px;
+                gap: 20px;
+            }
+            .movie img {
+                max-width: 100%;
+                border-radius: 12px;
+                transition: transform 0.2s ease-in-out;
+                box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+            }
+            .movie:hover img {
+                transform: scale(1.05) translateY(-10px);
+            }
+            .movie h4 {
+                font-size: 18px;
+                text-align: center;
+            }
+        `}</style>
         </>
     );
 }
