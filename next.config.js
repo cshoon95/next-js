e14ef467b06c0fdfa0bcc15ba67ab0e44c39b5e1
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-07-17 22:28:29 +0900

    리다이렉트 예제

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/content",
+        destination: "/about",
+        permanent: false
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
